fad480071a66b8dac4a3bd8714648ab4ff8144ba
Christina <yickind@hotmail.co.uk>
2023-10-29 15:42:23 +0000

    Added mongoose schema for user and diary entries

---
+++ server/src/models/Entries.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const EntrySchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    date: {type: Date, required: true},
+    imageUrl: {type: String},
+    entryText: {type: String, required: true},
+    vocabList: {type: String},
+    userOwner: {type: mongoose.Schema.Types.ObjectId, ref: "users", required:true}
+});
+
+export const EntryModel = mongoose.model("entries", EntrySchema);
